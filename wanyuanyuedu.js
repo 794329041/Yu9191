6016be3a257d89857610a1baa213368ee5001c4d
794329041 <794329041@qq.com>
2024-08-12 13:16:08 +0800

    Update wanyuanyuedu.js

--- wanyuanyuedu.js
+++ wanyuanyuedu.js
@@ -12,7 +12,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/wanyuanyuedu.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/794329041/Yu9191/main/wanyuanyuedu.js
 
 [mitm] 
 
